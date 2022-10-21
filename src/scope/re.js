fcd8e4fc30a9d54c9afb787c3a0aef5a7d26c2fe
sebastoss <sfpozzi@gmail.com>
2022-10-21 20:50:19 -0300

    Agrego archivos block.js, function.js, global.js y re.js dentro de la carpeta src.

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName; // Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName); // mostrar
+
+var secondName = 'David'; // declarar / asignar
+var secondName = 'Ana'; // reasignando
+console.log(secondName);
+
+// Let
+let fruit = 'Apple'; // Declarar y asginar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+
+// const
+const animal = 'Dog'; // Declara y asigna
+// animal = 'Gato'; // reasignado
+const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('Auto');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
